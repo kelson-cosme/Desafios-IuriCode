8f2d3abe7648a496747908ec92ef7dbaf7267853
kelson-almeida <kelson.almeida123@gmail.com>
2022-03-06 17:37:21 -0400

    att

--- desafio09/script.js
+++ desafio09/script.js
@@ -2,9 +2,20 @@ const menuMobile = document.getElementById("menuMobile");
 const lista = document.getElementById("menu");
 const tela = document.getElementById("tela");
 
+const linha1 = document.getElementById("line1");
+const linha2 = document.getElementById("line2");
+const linha3 = document.getElementById("line3");
+
 function aparecer(){
     lista.classList.toggle("active")
     tela.classList.toggle("cheio")
+
+
+    if(menuMobile.className == ""){
+        linha1.classList.toggle("ativar")
+        linha2.classList.toggle("ativar2")
+        linha3.classList.toggle("ativar3")
+    }
 
     if(tela.className == "cheio"){
         tela.addEventListener("click", aparecer)
