c12ed1384fecec6c1a1d2a1b80f31752ec627598
kelson-almeida <kelson.almeida123@gmail.com>
2022-03-20 19:04:20 -0400

    att

--- desafio20/exp.js
+++ desafio20/exp.js
@@ -18,38 +18,109 @@ function procurar(event){
     }
 }
 
+let direita = document.getElementById("direita")
 
 const texto = {
-    '1': () => resultado.innerHTML = "taa",
+    '1': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste1"+
+            "</h1>" +
     
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
     
-  
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
     
-    '2': () => resultado.innerHTML = "teste2",
-    '3': () => resultado.innerHTML = "teste3",
-    '4': () => resultado.innerHTML = "teste4",
-    '5': () => resultado.innerHTML = "teste5",
-    '6': () => resultado.innerHTML = "teste6",
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,    
+    
+    '2': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste2"+
+            "</h1>" +
+    
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
+    
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
+    
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,
+
+    
+    '3': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste3"+
+            "</h1>" +
+    
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
+    
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
+    
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,
+
+
+    '4': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste4"+
+            "</h1>" +
+    
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
+    
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
+    
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,
+
+
+    '5': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste5"+
+            "</h1>" +
+    
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
+    
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
+    
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,
+
+
+    '6': () => direita.innerHTML = `
+    ${
+        "<div class = 'titulo'>"+ 
+            "<h1 class = 'titulo2'>"
+                +"teste6"+
+            "</h1>" +
+    
+            "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
+        +"</div>"+
+    
+        "<p class = 'local'>"+ "Digital House" +"</p>"+
+    
+        "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
+    }
+    `,
 
 }
 // texto[1](); //Aparcer o primeiro resultado ao carregar a página
 lista.addEventListener("click", procurar);
-
-
-// let direita = document.getElementById("direita")
-
-// direita.innerHTML = `
-// ${
-//     "<div class = 'titulo'>"+ 
-//         "<h1 class = 'titulo2'>"
-//             +"Professor conteudista em Frontend"+
-//         "</h1>" +
-
-//         "<p class = 'data'>"+ 'Nov 2021 - Atual' + "</p>"
-//     +"</div>"+
-
-//     "<p class = 'local'>"+ "Digital House" +"</p>"+
-
-//     "<p class='funcao'>" + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at mattis metus. Nullam sit amet mauris condimentum, volutpat augue in, mattis urna." + "</p>"
-// }
-// `
